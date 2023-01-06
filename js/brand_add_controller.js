d5724c2096faea01aeda420019ce361263483148
Sandini Kaveesha <sandinikaveesha@gmail.com>
2023-01-06 23:45:55 +0530

    modified brand controller and category controller

--- js/brand_add_controller.js
+++ js/brand_add_controller.js
@@ -13,15 +13,17 @@ function save() {
             $('#description-error').css("display", "block");
         }
     } else {
+        var apiData = {
+            name: brand_name,
+            description: description,
+            status: "Active"
+        }
         $.ajax({
             type: 'POST',
             url: 'http://localhost:8080/brands/add',
-            cache: "false",
-            data: {
-                name: brand_name,
-                description: description,
-                status: "Active"
-            },
+            crossDomain: true,
+            contentType: "application/json",
+            data: JSON.stringify(apiData),
             success: (data, status) => {
                 if (status == 'success') {
                     $('#alert').css("display", "block");
@@ -44,14 +46,15 @@ function save() {
 }
 
 function lock(id) {
-    var id = id;
+    var apiData = {
+        brand: id
+    }
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/brands/brand/deactivate',
-        cache: "false",
-        data: {
-            number: id
-        },
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
         success: (data, status) => {
             if (status == "success") {
                 loadTable();
@@ -64,14 +67,15 @@ function lock(id) {
 }
 
 function unlock(id){
-    var id = id;
+    var apiData = {
+        brand_id: id
+    }
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/brands/brand/activate',
-        cache: "false",
-        data: {
-            number: id
-        },
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
         success: (data, status) => {
             if (status == "success") {
                 loadTable();
@@ -94,12 +98,96 @@ function loadTable() {
                 var brands = "";
                 var index = 1;
                 for (var i = 0; i < data.length; i++) {
-                    brands += '<tr><th scope="row">' + index + '</th><td>' + data[i].name + '</td><td>' + data[i].description + '</td><td>' + data[i].status + '</td><td width="150px"><button class="btn btn-sm btn-outline-dark px-3 ml-2 mr-1"><i class="fa fa-edit"></i></button><button class="btn btn-sm btn-outline-danger " id="' + data[i].id + 'lock" onclick="lock(' + data[i].id + ')" ><i class="fa fa-lock"></i></button><button  id="' + data[i].id + 'unlock" class="btn btn-sm btn-outline-success " onclick="unlock(' + data[i].id + ')"><i class="fa fa-unlock"></i></button></td></tr>'
+                    brands += '<tr><th scope="row">' + index + '</th><td>' + data[i].name + '</td><td>' + data[i].description + '</td><td>' + data[i].status + '</td><td width="150px"><button class="btn btn-sm btn-outline-dark px-3 ml-2 mr-1" onclick="editView('+data[i].id+')"><i class="fa fa-edit"></i></button><button class="btn btn-sm btn-outline-danger " id="' + data[i].id + 'lock" onclick="lock(' + data[i].id + ')" ><i class="fa fa-lock"></i></button><button  id="' + data[i].id + 'unlock" class="btn btn-sm btn-outline-success " onclick="unlock(' + data[i].id + ')"><i class="fa fa-unlock"></i></button></td></tr>'
                     index++;
                 }
                 $('#brand-table').html(brands);
             }
             console.log(status);
         }
+        
     })
+
+
+}
+
+function editView(id){
+    console.log("check");
+    var apiData = {
+        id: id
+    }
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/brands/brand',
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
+        success: (data, status) => {
+            if (status == "success") {
+                console.log(data);
+                $('.brand-name').val(data.name);
+                $('.brand-decs').val(data.description);
+                $('.brand-id').val(data.id);
+                $('.brand-status').val(data.status);
+                $('.add-header').removeClass("show");
+                $('.add-header').addClass("hide");
+                $('.edit-header').removeClass("hide");
+                $('.edit-header').addClass("show");
+                $('#btn-submit').removeClass("show");
+                $('#btn-submit').addClass("hide");
+                $('#btn-edit').addClass("show");
+                $('#btn-edit').removeClass("hide");
+            }
+            console.log(status);
+        }
+        
+    })
+}
+
+function edit() {
+ var id = $('.brand-id').val();
+ var name = $('.brand-name').val();
+ var desc = $('.brand-decs').val();
+ var sts = $('.brand-status').val();
+ var apiData ={
+    id: id,
+    name: name,
+    description: desc,
+    status: sts
+ }
+ $.ajax({
+    type: 'POST',
+    url: 'http://localhost:8080/brands/brand/update',
+    crossDomain: true,
+    contentType: "application/json",
+    data: JSON.stringify(apiData),
+    success: (data, status) => {
+        if (status == "success") {
+            $('#alert').css("display", "block");
+            $('#alert').addClass("alert-success");
+            $('#alert-title').text("Saved!");
+            $('#alert-info').text("Successfully updated Brand details "+data.name);    
+            loadTable();
+            $('.brand-name').val(null);
+            $('.brand-decs').val(null);
+            $('.brand-id').val(null);
+            $('.brand-status').val(null);
+            $('.add-header').removeClass("hide");
+            $('.add-header').addClass("show");
+            $('.edit-header').removeClass("show");
+            $('.edit-header').addClass("hide");
+            $('#btn-submit').removeClass("hide");
+            $('#btn-submit').addClass("show");
+            $('#btn-edit').addClass("hide");
+            $('#btn-edit').removeClass("show"); 
+        }else {
+            $('#alert').css("display", "block");
+            $('#alert').addClass("alert-warning");
+            $('#alert-title').text("Error!");
+            $('#alert-info').text("Something went wrong Please try again");
+        }
+        console.log(data);
+        console.log(status);
+    }
+})
 }
--- js/category_add_contoller.js
+++ js/category_add_contoller.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('.category-name').val(null);
+    $('.category-decs').val(null);
+    $('.category-id').val(null);
+    $('.category-stat').val(null);
     loadTable();
 });
 
@@ -14,24 +18,23 @@ function save(){
     }
    }
    else{
+    var apiData = {
+        name: category_name,
+        description: description,
+        status: "Active"
+    }
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/categories/add',
-        cache: "false",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: {
-            name: category_name,
-            description: description,
-            status: "Active"
-        },
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
         success: (data, status) => {
             if (status == 'success') {
                 $('#alert').css("display", "block");
                 $('#alert').addClass("alert-success");
                 $('#alert-title').text("Saved!");
-                $('#alert-info').text("Successfully Published Brand to the System");
+                $('#alert-info').text("Successfully Published Category to the System");
                 loadTable();
             } else {
                 $('#alert').css("display", "block");
@@ -47,14 +50,15 @@ function save(){
 }
 
 function lock(id) {
-    var id = id;
+    var apiData = {
+        id: id
+    }
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/categories/category/deactivate',
-        cache: "false",
-        data: {
-            number: id
-        },
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
         success: (data, status) => {
             if (status == "success") {
                 loadTable();
@@ -67,14 +71,15 @@ function lock(id) {
 }
 
 function unlock(id) {
-    var id = id;
+    var apiData = {
+        id : id,
+    }
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8080/categories/category/activate',
-        cache: "false",
-        data: {
-            number: id
-        },
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
         success: (data, status) => {
             if (status == "success") {
                 loadTable();
@@ -98,7 +103,7 @@ function loadTable(){
                 var brands = "";
                 var index = 1;
                 for (var i = 0; i < data.length; i++) {
-                    brands += '<tr><th scope="row">' + index + '</th><td>' + data[i].name + '</td><td>' + data[i].description + '</td><td>' + data[i].status + '</td><td width="150px"><button class="btn btn-sm btn-outline-dark px-3 ml-2 mr-1"><i class="fa fa-edit"></i></button><button class="btn btn-sm btn-outline-danger " id="' + data[i].id + 'lock" onclick="lock(' + data[i].id + ')" ><i class="fa fa-lock"></i></button><button  id="' + data[i].id + 'unlock" class="btn btn-sm btn-outline-success " onclick="unlock(' + data[i].id + ')"><i class="fa fa-unlock"></i></button></td></tr>'
+                    brands += '<tr><th scope="row">' + index + '</th><td>' + data[i].name + '</td><td>' + data[i].description + '</td><td>' + data[i].status + '</td><td width="150px"><button class="btn btn-sm btn-outline-dark px-3 ml-2 mr-1" onclick="editView('+data[i].id+')"><i class="fa fa-edit"></i></button><button class="btn btn-sm btn-outline-danger " id="' + data[i].id + 'lock" onclick="lock(' + data[i].id + ')" ><i class="fa fa-lock"></i></button><button  id="' + data[i].id + 'unlock" class="btn btn-sm btn-outline-success " onclick="unlock(' + data[i].id + ')"><i class="fa fa-unlock"></i></button></td></tr>'
                     index++;
                 }
                 $('#category-table').html(brands);
@@ -106,4 +111,87 @@ function loadTable(){
             console.log(status);
         }
     })
+}
+
+function editView(id){
+    console.log("check");
+    var apiData = {
+        id: id
+    }
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/categories/category',
+        crossDomain: true,
+        contentType: "application/json",
+        data: JSON.stringify(apiData),
+        success: (data, status) => {
+            if (status == "success") {
+            console.log(data);
+            $('.category-name').val(data.name);
+            $('.category-decs').val(data.description);
+            $('.category-id').val(data.id);
+            $('.category-stat').val(data.status);
+            $('.add-header').removeClass("show");
+            $('.add-header').addClass("hide");
+            $('.edit-header').removeClass("hide");
+            $('.edit-header').addClass("show");
+            $('#btn-submit').removeClass("show");
+            $('#btn-submit').addClass("hide");
+            $('#btn-edit').addClass("show");
+            $('#btn-edit').removeClass("hide");
+        
+        }
+        console.log(status);
+    }
+        
+    })
+}
+
+function edit(){
+    var id = $('.category-id').val();
+    var name = $('.category-name').val();
+    var desc = $('.category-decs').val();
+    var stat = $('.category-stat').val();
+    var apiData = {
+        id: id,
+        name: name,
+        description: desc,
+        status: stat
+    }
+    $.ajax({
+    type: 'POST',
+    url: 'http://localhost:8080/categories/category/update',
+    crossDomain: true,
+    contentType: "application/json",
+    data: JSON.stringify(apiData),
+    success: (data,status) => {
+        if(status == "success"){
+            $('#alert').css("display", "block");
+            $('#alert').addClass("alert-success");
+            $('#alert-title').text("Saved!");
+            $('#alert-info').text("Successfully updated category details "+data.name);    
+            loadTable();
+            $('.category-name').val(null);
+            $('.category-decs').val(null);
+            $('.category-id').val(null);
+            $('.category-stat').val(null);
+            $('.add-header').removeClass("hide");
+            $('.add-header').addClass("show");
+            $('.edit-header').removeClass("show");
+            $('.edit-header').addClass("hide");
+            $('#btn-submit').removeClass("hide");
+            $('#btn-submit').addClass("show");
+            $('#btn-edit').addClass("hide");
+            $('#btn-edit').removeClass("show");
+        }else {
+            $('#alert').css("display", "block");
+            $('#alert').addClass("alert-warning");
+            $('#alert-title').text("Error!");
+            $('#alert-info').text("Something went wrong Please try again");
+        }
+        console.log(data);
+        console.log(status);
+    }
+    })
+        
 }
